d1320759ffc450a871bad9584d1b642d0f860826
0Shie0 <43030810+0Shie0@users.noreply.github.com>
2018-10-11 13:32:38 +0300

    Add files via upload

---
+++ 11.10.18/robot-tasks-master/task_9.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+from pyrob.api import *
+
+
+@task
+def task_8_2():
+    if wall_is_above() == False:
+            fill_cell()
+    while  wall_is_on_the_right() == False:
+        move_right()
+        if wall_is_above() == False:
+            fill_cell()
+    pass
+
+
+if __name__ == '__main__':
+    run_tasks()
